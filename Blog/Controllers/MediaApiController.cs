5e4e36fb30de519ee283f138b1699bf29bb68760
osija <kaydarovs@mail.ru>
2022-07-20 22:03:08 +0300

    add media api

---
+++ Blog/Controllers/MediaApiController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MediaApiController : ControllerBase
+    {
+        public string folder = "wwwroot/media/";
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            var folderInfo = new DirectoryInfo(folder);
+            var filesInfo = folderInfo.GetFiles();
+            var filesName = filesInfo.Select(f => f.Name).OrderByDescending(t => t).ToList();
+            return filesName;
+        }
+
+        [HttpPost]
+        public async Task<bool> Post(IFormFile iForm)
+        {
+            if(iForm == null)
+            {
+                return false;
+            }
+
+            bool status;
+            try
+            {
+                var filename = folder + DateTime.Now.Ticks.ToString() + iForm.FileName;
+                using(var stream = System.IO.File.Create(filename))
+                {
+                    await iForm.CopyToAsync(stream);
+                }
+
+                status = true;
+            }
+            catch(Exception e)
+            {
+                status = false;
+            }
+            return status;
+        }
+    }
+}
---
+++ Blog/wwwroot/js/mediaservice.js
@@ -0,0 +1,75 @@
+﻿
+const url = '/MediaApi';
+const mediaFolder = '/media/';
+
+
+
+async function images_popUp() {
+    var popUp = document.getElementById('PopUp_Images');
+    popUp.hidden = false;
+    await render_images();
+}
+
+function close_images_popUp() {
+    var popUp = document.getElementById('PopUp_Images');
+    popUp.hidden = true;
+}
+
+async function get_media() {
+
+    let res = await fetch(url, {
+        method: 'GET'
+    });
+
+    return await res.json();
+}
+
+async function save_image() {
+    var form = document.getElementById('new_file');
+    var data = new FormData();
+    data.append('iForm', form.files[0]);
+
+    await fetch(url, {
+        method: 'POST',
+        //headers: { 'Content-type': 'multipart/form-data' },
+        body: data
+    }).then(function (response) {
+        if (response.status !== 200) {
+            console.log('Bad status: ' + response.status);
+            return;
+        }
+        response.json().then(function (status) {
+            console.log(status);
+            form.value = '';
+        });
+    }).catch(function (err) {
+        console.log('Fetch Error: -S', err);
+    });
+    await render_images();
+}
+
+async function render_images() {
+    let data = await get_media();
+    let media_area = document.getElementById('images_list');
+    media_area.innerHTML = '';
+
+    
+
+    for (i = 0; i < data.length; i++) {
+        let img = document.createElement('img');
+        let src = mediaFolder + data[i];
+        img.src = src;
+        img.className = 'media_preview';
+        img.onclick = function () { select_image(src) };
+        media_area.appendChild(img);
+        
+    }
+}
+
+function select_image(filepath) {
+    let text_area = document.getElementById('input_field');
+    let start_carriage = text_area.selectionStart;
+    let text = text_area.value.substring(0, start_carriage) + "[img]" + filepath + "[/img]" + text_area.value.substring(start_carriage);
+    text_area.value = text;
+    close_images_popUp();
+}
